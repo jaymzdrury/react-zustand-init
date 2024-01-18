72df240d3c01cd88f63a6ec1e39e3b7655d65e68
James Drury <57101727+jaymzdrury@users.noreply.github.com>
2024-01-18 06:08:49 -0500

    migrate to vite

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-createRoot(document.getElementById('root') as HTMLElement).render(<App />)
-reportWebVitals();
